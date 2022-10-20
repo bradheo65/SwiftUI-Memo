165998cd0e5ba4432d36579f489a085af171c118
brad <gjrjs31@naver.com>
2022-10-20 16:15:43 +0900

    feat: MainListView의 View 추가 및 상세 옵션 설정
    - NavigationView 추가
    - 각 Text 별 상세 옵션 설정

--- SwiftUI-Memo/SwiftUI-Memo/MainListView.swift
+++ SwiftUI-Memo/SwiftUI-Memo/MainListView.swift
@@ -11,10 +11,22 @@ struct MainListView: View {
     @EnvironmentObject var store: MemoStore 
     
     var body: some View {
-        List(store.list) { memo in
-            Text(memo.content)
-            
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertData, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
         }
+        
     }
 }
 
