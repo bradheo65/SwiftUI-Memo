1cbfa988fd6cb96b4aee547f034bed65dc1a3551
brad <gjrjs31@naver.com>
2022-10-20 16:09:05 +0900

    feat: 더미 데이터 List View로 구현

--- SwiftUI-Memo/SwiftUI-Memo/MainListView.swift
+++ SwiftUI-Memo/SwiftUI-Memo/MainListView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore 
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(store.list) { memo in
+            Text(memo.content)
+            
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
